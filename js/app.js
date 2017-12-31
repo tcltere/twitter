ae931636b3ba396892cc4af43eef799280306b03
Teresita Cañete <teresitacanete@MacBook-Air-de-Teresita.local>
2017-12-30 22:51:52 -0300

    correccion twittwr

--- js/app.js
+++ js/app.js
@@ -1,13 +1,35 @@
+document.getElementById("btn").addEventListener("click", add);
+contador();
+
 function add(){
-	var mensaje = document.getElementById("mensaje").value;
+	var mensaje = document.getElementById("escribir").value;
 
-	document.getElementById("mensaje").value = "";
+	document.getElementById("escribir").value = "";
+	document.getElementById("cont").textContent = "140";
 
-	var cont = documet.getElementById("cont");
+
+	var comment = document.getElementById("comment");
+
 
 	if(mensaje.length == 0 || mensaje == null) {
 		alert('Debes ingresar un mensaje');
 		return false;
+
+    }
+
+    var nodo = document.createTextNode(mensaje);
+    var parrafo = document.createElement("p");
+    var contenedor = document.createElement("div");
+    parrafo.appendChild(nodo);
+    contenedor.appendChild(parrafo);
+    comment.appendChild(contenedor);
+}
+document.getElementById("escribir").addEventListener("keydown", contador);
+function contador(){
+	var contar = document.getElementById("escribir").value.length;
+	var cont = document.getElementById("cont");
+    var nodo1 = document.createTextNode(contar);
+	cont.textContent = 140-contar;
+    
+
 	}
-	
-	
